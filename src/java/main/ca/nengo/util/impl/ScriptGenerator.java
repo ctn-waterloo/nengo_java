d623459473136200f77309eb64d65968b90dbbe8
hunse <hunse@ctn>
2012-11-29 17:59:43 -0500

    Suppressed some warnings

--- src/java/main/ca/nengo/util/impl/ScriptGenerator.java
+++ src/java/main/ca/nengo/util/impl/ScriptGenerator.java
@@ -48,6 +48,7 @@ public class ScriptGenerator extends DFSIterator {
         return super.startDFS(node);
     }
 	
+    @SuppressWarnings("unchecked")
 	protected void pre(Node node) {
         if (parentNetwork.peek().getMetaData("templates") != null &&
         	    ((ArrayList)parentNetwork.peek().getMetaData("templates")).contains(node.getName())) 
@@ -95,6 +96,7 @@ public class ScriptGenerator extends DFSIterator {
         }
 	}
 	
+    @SuppressWarnings("unchecked")
 	protected void post(Node node)
 	{
         if (node instanceof Network && inTemplateNetwork <= 0)
